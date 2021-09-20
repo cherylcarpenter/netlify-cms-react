928117bdcc9d269bc0a34dde55dfde94a169c96b
Cheryl Carpenter <cherylcarpenter2015@gmail.com>
2021-09-19 19:53:38 -0500

    working on manual config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,18 +1,20 @@
 import CMS from 'netlify-cms-app';
 import pages from './pages/index';
 import posts from './collections/posts';
+import { init } from 'netlify-cms-app';
 
 window.CMS_MANUAL_INIT = true;
+const { BRANCH } = process.env;
 
-CMS.init({
-  config: {
-    load_config_file: false,
-    backend: {
-      name: 'git-gateway',
-      branch: 'master',
-    },
-    media_folder: '/static/img',
-    public_folder: '/img',
-    collections: [pages, posts],
+const config = {
+  load_config_file: false,
+  backend: {
+    name: 'github',
+    branch: BRANCH || 'master',
   },
-});
+  media_folder: '/static/img',
+  public_folder: '/img',
+  collections: [pages, posts],
+};
+
+init({ config });
